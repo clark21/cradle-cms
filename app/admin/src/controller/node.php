67760b03a8433f26c97abf5d9a589994530462d4
Charles Zamora <charles.andacc@gmail.com>
2018-02-07 19:15:39 +0800

    node create and update updates

--- app/admin/src/controller/node.php
+++ app/admin/src/controller/node.php
@@ -470,6 +470,108 @@ $cradle->get('/admin/node/:node_type/create', function($request, $response) {
 }, 'render-admin-page');
 
 /**
+ * Render dynamic node update
+ * 
+ * @param Request $request
+ * @param Response $response
+ */
+$cradle->get('/admin/node/:node_type/update/:node_id', function($request, $response) {
+    //----------------------------//
+    // 1. Route Permissions
+    //only for admin
+    cradle('global')->requireLogin('admin');
+
+    // validate meta
+    cradle()->trigger('meta-validate', $request, $response);
+
+    // error?
+    if($response->isError() && $response->getMessage() === 'Not Found') {
+        // do nothing
+        return;
+    }
+
+    // get the meta
+    $meta = $response->getResults();
+
+    //----------------------------//
+    // 2. Prepare Data
+    $data = ['item' => $request->getPost()];
+
+    //add CSRF
+    cradle()->trigger('csrf-load', $request, $response);
+    $data['csrf'] = $response->getResults('csrf');
+
+    //add CDN
+    $config = $this->package('global')->service('s3-main');
+    $data['cdn_config'] = File::getS3Client($config);
+
+    //if no item
+    if(empty($data['item'])) {
+        // get node detail
+        cradle()->trigger('node-detail', $request, $response);
+
+        //can we update ?
+        if($response->isError()) {
+            //add a flash
+            cradle('global')->flash($response->getMessage(), 'danger');
+
+            return cradle('global')->redirect(
+                sprintf(
+                    '/admin/node/%s/search',
+                    $request->getStage('node_type')
+                )
+            );
+        }
+
+        // get results
+        $data['item'] = $response->getResults();
+
+        // merge meta with item
+        $data['item'] = array_merge($data['item'], $data['item']['node_meta']);
+    }
+
+    // set errors
+    $data['errors'] = [];
+
+    // error?
+    if ($response->isError()) {
+        $response->setFlash($response->getMessage(), 'danger');
+        $data['errors'] = $response->getValidation();
+    }
+
+    // set meta data
+    $data['meta'] = $meta;
+
+    // set fields template
+    $data['fields'] = (new Fields($meta['meta_fields']))
+        ->setData($data['item'])
+        ->setError($data['errors'])
+        ->compile();
+
+    //----------------------------//
+    // 3. Render Template
+    
+    $class = sprintf(
+        'page-developer-node-%s-update page-admin',
+        $meta['meta_key']
+    );
+
+    $data['title'] = cradle('global')->translate(
+        'Updating %s',
+        ucwords($meta['meta_singular'])
+    );
+
+    $body = cradle('/app/admin')->template('node/type-form', $data);
+
+    //set content
+    $response
+        ->setPage('title', $data['title'])
+        ->setPage('class', $class)
+        ->setContent($body);
+
+}, 'render-admin-page');
+
+/**
  * Process dynamic node create
  * 
  * @param Request $request
@@ -561,13 +663,13 @@ $cradle->post('/admin/node/:node_type/create', function($request, $response) {
         $request->setStage('node_published', null);
     }
 
-    //node_type is disallowed
-    $request->removeStage('node_type');
-
     //if node_flag has no value make it null
     if ($request->hasStage('node_flag') && !$request->getStage('node_flag')) {
         $request->setStage('node_flag', null);
     }
+
+    // get user
+    $request->setStage('user_id', $request->getSession('me', 'user_id'));
 
     //----------------------------//
     // 3. Process Request
@@ -580,7 +682,7 @@ $cradle->post('/admin/node/:node_type/create', function($request, $response) {
         return cradle()
             ->triggerRoute(
                 'get',
-                sprintf('/admin/node/%s/create', $meta['meta_key']),
+                sprintf('/admin/node/%s/create', $request->getStage('node_type')),
                 $request,
                 $response
             );
@@ -594,7 +696,142 @@ $cradle->post('/admin/node/:node_type/create', function($request, $response) {
     return cradle('global')->redirect(
         sprintf(
             '/admin/node/%s/search',
-            $meta['meta_key']
+            $request->getStage('node_type')
+        )
+    );
+});
+
+/**
+ * Process dynamic node update
+ * 
+ * @param Request $request
+ * @param Response $response
+ */
+$cradle->post('/admin/node/:node_type/update/:node_id', function($request, $response) {
+    //----------------------------//
+    // 1. Route Permissions
+    //only for admin
+    cradle('global')->requireLogin('admin');
+
+    // validate meta
+    cradle()->trigger('meta-validate', $request, $response);
+
+    // error?
+    if($response->isError()) {
+        // do nothing
+        return;
+    }
+
+    // get the data
+    $data = $request->getStage();
+
+    // get the meta
+    $meta = $response->getResults();
+
+    //----------------------------//
+    // 2. Prepare Data
+    $fields = (new Fields($meta['meta_fields']))
+        ->setData($data);
+
+    // get errors
+    $errors = $fields->getValidation();
+
+    // if we have errors
+    if(!empty($errors)) {
+        $response
+            ->setError(true, 'Invalid Request')
+            ->set('json', 'validation', $errors);
+
+        // trigger route
+        return cradle()
+            ->triggerRoute(
+                'get',
+                sprintf(
+                    '/admin/node/%s/update/%s', 
+                    $request->getStage('node_type'),
+                    $request->getStage('node_id')
+                ),
+                $request,
+                $response
+            );
+    }
+
+    // get field values (non-default)
+    $values = $fields->getValues();
+
+    // values to stage
+    $request->setStage('node_meta', $values);
+
+    //if node_image has no value make it null
+    if ($request->hasStage('node_image') && !$request->getStage('node_image')) {
+        $request->setStage('node_image', null);
+    }
+
+    //if node_detail has no value make it null
+    if ($request->hasStage('node_detail') && !$request->getStage('node_detail')) {
+        $request->setStage('node_detail', null);
+    }
+
+    //if node_tags has no value make it null
+    if ($request->hasStage('node_tags') && !$request->getStage('node_tags')) {
+        $request->setStage('node_tags', null);
+    }
+
+    //if node_meta has no value make it null
+    if ($request->hasStage('node_meta') && !$request->getStage('node_meta')) {
+        $request->setStage('node_meta', null);
+    }
+
+    //if node_files has no value make it null
+    if ($request->hasStage('node_files') && !$request->getStage('node_files')) {
+        $request->setStage('node_files', null);
+    }
+
+    //if node_status has no value use the default value
+    if ($request->hasStage('node_status') && !$request->getStage('node_status')) {
+        $request->setStage('node_status', 'pending');
+    }
+
+    //if node_published has no value make it null
+    if ($request->hasStage('node_published') && !$request->getStage('node_published')) {
+        $request->setStage('node_published', null);
+    }
+
+    //if node_flag has no value make it null
+    if ($request->hasStage('node_flag') && !$request->getStage('node_flag')) {
+        $request->setStage('node_flag', null);
+    }
+
+    //----------------------------//
+    // 3. Process Request
+    cradle()->trigger('node-update', $request, $response);
+
+    //----------------------------//
+    // 4. Interpret Results
+    if($response->isError()) {
+        // trigger route
+        return cradle()
+            ->triggerRoute(
+                'get',
+                sprintf(
+                    '/admin/node/%s/update/%s', 
+                    $request->getStage('node_type'),
+                    $request->getStage('node_id')
+                ),
+                $request,
+                $response
+            );
+    }
+
+    //it was good
+    //add a flash
+    cradle('global')->flash('Node was Updated', 'success');
+
+    //redirect
+    return cradle('global')->redirect(
+        sprintf(
+            '/admin/node/%s/search',
+            $request->getStage('node_type')
         )
     );
 });
