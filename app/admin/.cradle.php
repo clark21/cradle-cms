963e807146055cb675422aea12d06193730fd287
Charles Zamora <charles.andacc@gmail.com>
2018-02-08 16:04:18 +0800

    archive node module and other stuff

--- app/admin/.cradle.php
+++ app/admin/.cradle.php
@@ -4,7 +4,6 @@
 //START: GENERATED CONTROLLERS
 include_once __DIR__ . '/src/controller/auth.php';
 include_once __DIR__ . '/src/controller/user.php';
-include_once __DIR__ . '/src/controller/node.php';
 include_once __DIR__ . '/src/controller/meta.php';
 //END: GENERATED CONTROLLERS
 
