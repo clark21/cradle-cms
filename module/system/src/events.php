2fb87f58d5aaee028fd333d315a03b08530bb057
mnlcandelaria <mnl.candelaria@gmail.com>
2018-02-15 18:39:10 +0800

    create and update schema validation

--- module/system/src/events.php
+++ module/system/src/events.php
@@ -37,12 +37,12 @@ $cradle->on('system-schema-create', function ($request, $response) {
 
     //----------------------------//
     // 3. Prepare Data
-    
+
     // filter relations
     if(isset($data['relations'])) {
         // filter out empty relations
         $data['relations'] = array_filter(
-            $data['relations'], 
+            $data['relations'],
             function($relation) {
                 // make sure we have relation name
                 return $relation['name'] !== '' ? true : false;
