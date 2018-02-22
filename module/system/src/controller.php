3de2199928175622cdc60565e40d33e00750b63b
Christian Blanquera <cblanquera@openovate.com>
2018-02-22 23:09:17 +0800

    system documentation ~3h
    - moved body including crumbs to body templates. please update.
    - added footer
    - updated navigation, cased for 3 teirs
    - navigation now knows whats active
    - ported over the admin theme templates. visit `/admin/system/template/search`
    - provisioned for docs visit `/admin/system/docs`

--- module/system/src/controller.php
+++ module/system/src/controller.php
@@ -388,3 +388,60 @@ $cradle->get('/admin/system/schema/restore/:name', function($request, $response)
 
     cradle('global')->redirect('/admin/system/schema/search');
 });
+
+
+/**
+ * Render Template Actions
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$cradle->get('/admin/system/template/:action', function($request, $response) {
+    //----------------------------//
+    // 1. Route Permissions
+    //only for admin
+    cradle('global')->requireLogin('admin');
+
+    $action = $request->getStage('action');
+
+    //----------------------------//
+    // 2. Render Template
+    $class = 'page-admin-system-template-' . $action . ' page-admin';
+    $data['title'] = cradle('global')->translate('System Template ' . ucfirst($action));
+    $body = cradle('/module/system')->template('template/' . $action, $data);
+
+    //set content
+    $response
+        ->setPage('title', $data['title'])
+        ->setPage('class', $class)
+        ->setContent($body);
+
+    //render page
+}, 'render-admin-page');
+
+/**
+ * Render Docs Actions
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$cradle->get('/admin/system/docs', function($request, $response) {
+    //----------------------------//
+    // 1. Route Permissions
+    //only for admin
+    cradle('global')->requireLogin('admin');
+
+    //----------------------------//
+    // 2. Render Template
+    $class = 'page-admin-system-docs page-admin';
+    $data['title'] = cradle('global')->translate('Documentation');
+    $body = cradle('/module/system')->template('docs', $data);
+
+    //set content
+    $response
+        ->setPage('title', $data['title'])
+        ->setPage('class', $class)
+        ->setContent($body);
+
+    //render page
+}, 'render-admin-page');
