3a2c681b3c8191668dc9bc06367909586f135dfb
Christian Blanquera <cblanquera@openovate.com>
2018-03-02 23:49:14 +0800

    Files and big data ~1h
     - Moved big data to a JSON folder
     - Made the Utility File class also use the JSON file (becareful when editing this)
     - Added a dependency loader on the bottom of admin.js

--- module/utility/.cradle.php
+++ module/utility/.cradle.php
@@ -1,1 +1,6 @@
 <?php //-->
+$cradle->preprocess(function() {
+    $extensions = $this->package('global')->path('public') . '/json/extensions.json';
+    $json = file_get_contents($extensions);
+    Cradle\Module\Utility\File::$extensions = json_decode($json, true);
+});
